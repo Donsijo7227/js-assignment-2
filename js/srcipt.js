a014137416bffbd9f95c6b460ceb864e5e69235e
donsi <donsi@DON>
2024-03-22 00:11:12 -0500

    added jscode for getting values from the form

---
+++ js/srcipt.js
@@ -0,0 +1,12 @@
+function orderSmoothie() {
+  const flavor = document.getElementById('flavor').value;
+  const size = document.getElementById('size').value;
+  const toppings = document.getElementById('toppings').value;
+  const specialInstructions = document.getElementById('specialInstructions').value;
+
+  console.log(flavor);
+  console.log(size);
+  console.log(toppings);
+  console.log(specialInstructions);
+
+}
